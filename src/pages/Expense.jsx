5dde3ebe73d62dd0bb1c64d7097739d90b9e71a4
Rabbil khan <rabbilkhan125@gmail.com>
2023-11-09 16:09:35 +0530

    database intergrate with expenses

--- src/pages/Expense.jsx
+++ src/pages/Expense.jsx
@@ -1,24 +1,125 @@
+import { useState, useEffect } from "react";
 
 const Expense = () => {
+  const [amount, setAmount] = useState("");
+  const [description, setDescription] = useState("");
+  const [expense, setExpense] = useState([]);
+  const [addrtype, setAddrtype] = useState([ ]);
+
+  
+
+  const fetchExpenseData = async () => {
+    try {
+      const response = await fetch(
+        "https://expensetracker-d8a79-default-rtdb.firebaseio.com/userid.json"
+      );
+      if (response.ok) {
+        const data = await response.json();
+        if (data) {
+          const expenseArray = Object.values(data);
+          setExpense(expenseArray);
+        }
+      } else {
+        console.error("Failed to fetch data");
+      }
+    } catch (error) {
+      console.error("Error while fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    // Fetch data when the component mounts
+    fetchExpenseData();
+  }, []);
+
+  const Formsubmit = async (e) => {
+    e.preventDefault();
+    const newExpense = {
+      amount: amount,
+      description: description,
+      addrtype: addrtype, 
+    };
+    console.log(addrtype)
+  
+    const res = await fetch(
+      "https://expensetracker-d8a79-default-rtdb.firebaseio.com/userid.json",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          amount,
+          description,
+          addrtype: newExpense.addrtype,
+          returnSecureToken: true,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    if (res.ok) {
+      alert("Successfully updated");
+  
+      // Update the local state with the new expense
+      setExpense([...expense, newExpense]);
+  
+      // Clear the input fields
+      setAmount("");
+      setDescription("");
+    }
+  };
+
   return (
-<>
-<h1 className="text-center">Expense Adding</h1>
-    <form className="flex justify-center my-2">
-      <input className="border-2 border-slate-950 px-2 mr-2" 
-      type="text" placeholder="Enter money"/>
-      <input className="border-2 border-slate-950 px-2 mr-2" type="text"
-       placeholder="Description"/>
-      <select className="border-2 border-slate-950 px-2 mr-2">
-        <option>Food</option>
-        <option>Petrol</option>
-        <option>Salary</option>
-      </select>
-      <button className="border-2 border-slate-950 px-2 mr-2 bg-slate-950 text-slate-50 rounded-sm">Add Expense</button>
-    </form>
+    <>
+      <h1 className="text-center">Expense Adding</h1>
+      {/* ... your input fields and select box ... */}
+      <form className="flex justify-center my-2" onSubmit={Formsubmit} method="Post">
+        <input
+          className="border-2 border-slate-950 px-2 mr-2"
+          type="text"
+          placeholder="Enter money"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
+        <input
+          className="border-2 border-slate-950 px-2 mr-2"
+          type="text"
+          placeholder="Description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <select
+          className="border-2 border-slate-950 px-2 mr-2"
+          onChange={(e) => setAddrtype(e.target.value)}
+        >
+          <option>food</option>
+          <option>mobile</option>
+          <option>cloths</option>
+        </select>
+        <button className="border-2 border-slate-950 px-2 mr-2 bg-slate-950 text-slate-50 rounded-sm">
+          Add Expense
+        </button>
+      </form>
 
+      <table className="m-auto">
+        <thead>
+          <tr className="flex justify-center my-2 gap-40">
+            <th>Amount</th>
+            <th>Description</th>
+            <th>Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {expense.map((expenses, index) => (
+            <tr key={index} className="flex justify-center my-2 gap-40">
+              <td>{expenses.amount}</td>
+              <td>{expenses.description}</td>
+              <td>{expenses.addrtype}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
 
-</>
-  )
-}
-
-export default Expense
+export default Expense;
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,113 +1,114 @@
 import { useContext, useEffect, useState } from "react";
 import AuthContext from "../authcontext/authcontext";
+
 const Profile = () => {
-    const authCtx = useContext(AuthContext);
-    const [name, setName] = useState('');
-    const [profileImage, setProfileImage] = useState('');
-    const [error, setError] = useState(null);
-    const [successMessage, setSuccessMessage] = useState(null);
+  const authCtx = useContext(AuthContext);
+  const [name, setName] = useState('');
+  const [profileImage, setProfileImage] = useState('');
+  const [error, setError] = useState(null);
+  const [successMessage, setSuccessMessage] = useState(null);
 
-    // Use local storage to initialize the form fields when the component mounts
-    useEffect(() => {
-        const storedData = JSON.parse(localStorage.getItem('userData'));
-        if (storedData) {
-            setName(storedData.name);
-            setProfileImage(storedData.profileImage);
-        }
-    }, []);
+  // Use local storage to initialize the form fields when the component mounts
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem('userData'));
+    if (storedData) {
+      setName(storedData.name);
+      setProfileImage(storedData.profileImage);
+    }
+  }, []);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        // Check if the user is authenticated
-        if (!authCtx.token) {
-            setError("User is not authenticated. Please log in.");
-            return;
-        }
-
-        try {
-            const requestBody = {
-                name,
-                profileImage,
-                returnSecureToken: true,
-                idToken: authCtx.token
-            };
-
-            // Send a POST request to update the profile
-            const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyBOHmI4S1eeBK4wrP1WlGvI-JosSRP8YCQ', {
-                method: "POST",
-                body: JSON.stringify(requestBody),
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            });
-
-            if (response.ok) {
-                setSuccessMessage("Profile updated successfully");
-                setError(null);
-
-                // Save the updated data in local storage
-                const updatedData = {
-                    name,
-                    profileImage,
-                };
-                localStorage.setItem('userData', JSON.stringify(updatedData));
-            } else {
-                const data = await response.json();
-                setError(data.error.message);
-                setSuccessMessage(null);
-            }
-        } catch (error) {
-            setError("An error occurred while updating the profile.");
-            setSuccessMessage(null);
-        }
+    // Check if the user is authenticated
+    if (!authCtx.token) {
+      setError("User is not authenticated. Please log in.");
+      return;
     }
 
-    return (
-        <>
-            <div className="block justify-center text-center">
-                <h1 className="text-lg text-red-500 font-extrabold">Profile Details</h1>
-            </div>
-            <div>
-                <form onSubmit={handleSubmit} className="lg:flex my-11 lg:justify-center gap-2 mx-5">
-                    <div>
-                        <label htmlFor="fullname">Full Name: &nbsp; &nbsp;</label>
-                        <input
-                            className="border-2 w-80 h-10 px-2"
-                            type="text"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                            placeholder="Enter Full Name"
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="profilephoto">Profile Photo URL &nbsp; &nbsp;</label>
-                        <input
-                            className="border-2 w-80 h-10 mx-2 px-2"
-                            value={profileImage}
-                            onChange={(e) => setProfileImage(e.target.value)}
-                            type="text"
-                            placeholder="Enter Image URL"
-                        />
-                    </div>
-                    <div>
-                        <button type="submit" className="border-2 bg-red-600 border-red-400 px-5 py-2 text-white">
-                            Update
-                        </button>
-                    </div>
-                    <div>
-                        <button className="border-2 border-red-400 px-5 py-2">
-                            Cancel
-                        </button>
-                    </div>
-                </form>
-                {error && <p className="text-red-500">{error}</p>}
-                {successMessage && <p className="text-green-500 text-center text-lg">{successMessage}</p>}
+    try {
+      const requestBody = {
+        name,
+        profileImage,
+        // You may need to send your user identifier here depending on your API
+        userId: authCtx.userId,
+        // You should have a suitable API URL for updating the user's profile
+      };
 
-            </div>
-          
-        </>
-    );
+      // Send a POST request to update the profile
+      const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyBOHmI4S1eeBK4wrP1WlGvI-JosSRP8YCQ/userId.json', {
+        method: "GET",
+        body: JSON.stringify(requestBody),
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${authCtx.token}`, // Include user token in headers
+        },
+      });
+
+      if (response.ok) {
+        setSuccessMessage("Profile updated successfully");
+        setError(null);
+
+        // Save the updated data in local storage
+        const updatedData = {
+          name,
+          profileImage,
+        };
+        localStorage.setItem('userData', JSON.stringify(updatedData));
+      } else {
+        const data = await response.json();
+        setError(data.error.message);
+        setSuccessMessage(null);
+      }
+    } catch (error) {
+      setError("An error occurred while updating the profile.");
+      setSuccessMessage(null);
+    }
+  }
+
+  return (
+    <>
+      <div className="block justify-center text-center">
+        <h1 className="text-lg text-red-500 font-extrabold">Profile Details</h1>
+      </div>
+      <div>
+        <form onSubmit={handleSubmit} className="lg:flex my-11 lg:justify-center gap-2 mx-5">
+          <div>
+            <label htmlFor="fullname">Full Name: &nbsp; &nbsp;</label>
+            <input
+              className="border-2 w-80 h-10 px-2"
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              placeholder="Enter Full Name"
+            />
+          </div>
+          <div>
+            <label htmlFor="profilephoto">Profile Photo URL &nbsp; &nbsp;</label>
+            <input
+              className="border-2 w-80 h-10 mx-2 px-2"
+              value={profileImage}
+              onChange={(e) => setProfileImage(e.target.value)}
+              type="text"
+              placeholder="Enter Image URL"
+            />
+          </div>
+          <div>
+            <button type="submit" className="border-2 bg-red-600 border-red-400 px-5 py-2 text-white">
+              Update
+            </button>
+          </div>
+          <div>
+            <button className="border-2 border-red-400 px-5 py-2">
+              Cancel
+            </button>
+          </div>
+        </form>
+        {error && <p className="text-red-500">{error}</p>}
+        {successMessage && <p className="text-green-500 text-center text-lg">{successMessage}</p>}
+      </div>
+    </>
+  );
 };
 
 export default Profile;
